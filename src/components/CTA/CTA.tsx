3d2774c7d6da646937d9f550b80f6ea6ef2f09e0
Daniel <kcomtdaniel@gmail.com>
2024-06-07 17:05:31 -0500

    added CTA

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -1,10 +1,28 @@
 import { Button, Wrapper } from "./CTA.styles";
 
 export function CTA() {
+  const handleDownload = () => {
+    const fileURL = `src/assets/rosamaria.vcf`;
+    const link = document.createElement("a");
+    link.href = fileURL;
+    link.download = "rosamaria.vcf";
+    link.click();
+  };
+
+  const redirectWhatsapp = () => {
+    const newURL = "https://wa.link/2q2tzs";
+    const link = document.createElement("a");
+    link.href = newURL;
+    link.target = "_blank";
+    link.click();
+  };
+
   return (
     <Wrapper>
-      <Button>Añadir Contacto</Button>
-      <Button $primary>Escríbeme</Button>
+      <Button onClick={handleDownload}>Añadir Contacto</Button>
+      <Button onClick={redirectWhatsapp} $primary>
+        Escríbeme
+      </Button>
     </Wrapper>
   );
 }
