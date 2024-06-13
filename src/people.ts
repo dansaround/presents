0fc29d076c0a577da6019a792e59ef65943b7dea
Daniel <kcomtdaniel@gmail.com>
2024-06-13 01:30:45 -0500

    moved images to public folder

--- src/people.ts
+++ src/people.ts
@@ -28,7 +28,7 @@ export const people = [
       properties: [
         {
           url: "https://www.remax.pe/web/search/property/propiedad-casa-en-venta-la-molina-lima-lima-1096139/",
-          image: "src/assets/rma/casa_la_molina.jpg",
+          image: "public/rma/casa_la_molina.jpg",
           title: "CASA URBANA EN VENTA",
           priceSol: "S/. 1'757,800.00",
           priceUsd: "USD 470,000.00",
@@ -36,7 +36,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-terreno-en-alquiler-chorrillos-lima-lima-1096089/",
-          image: "src/assets/rma/terreno_en_alquiler.jpg",
+          image: "public/rma/terreno_en_alquiler.jpg",
           title: "TERRENO COMERCIAL EN ALQUILER",
           priceSol: "S/. 11,220.00",
           priceUsd: "USD 3,000.00",
@@ -44,7 +44,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-miraflores-lima-lima-1092314/",
-          image: "src/assets/rma/depa_duplex_venta.jpg",
+          image: "public/rma/depa_duplex_venta.jpg",
           title: "DEPARTAMENTO DUPLEX EN VENTA",
           priceSol: "S/. 935,000.00",
           priceUsd: "USD 250,000.00",
@@ -52,7 +52,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-alquiler-san-isidro-lima-lima-1092222/",
-          image: "src/assets/rma/depa_flat_alquiler.jpg",
+          image: "public/rma/depa_flat_alquiler.jpg",
           title: "DEPARTAMENTO FLAT EN ALQUILER",
           priceSol: "S/. 4,862.00",
           priceUsd: "USD 1,300.00",
@@ -60,7 +60,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-miraflores-lima-lima-1089713/",
-          image: "src/assets/rma/depa_flat_venta.jpg",
+          image: "public/rma/depa_flat_venta.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 1'795,200.00",
           priceUsd: "USD 480,000.00",
@@ -68,7 +68,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-surquillo-lima-lima-1084225/",
-          image: "src/assets/rma/depa_flat_venta_surquillo.jpg",
+          image: "public/rma/depa_flat_venta_surquillo.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 299,200.00",
           priceUsd: "USD 80,000.00",
@@ -76,7 +76,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-alquiler-san-isidro-lima-lima-1082111/",
-          image: "src/assets/rma/depa_flat_alquiler_san_isidro.jpg",
+          image: "public/rma/depa_flat_alquiler_san_isidro.jpg",
           title: "DEPARTAMENTO FLAT EN ALQUILER",
           priceSol: "S/. 4,675.00",
           priceUsd: "USD 1,250.00",
@@ -84,7 +84,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-terreno-en-alquiler-chorrillos-lima-lima-1082081/",
-          image: "src/assets/rma/terreno_alquiler_chorrillos.jpg",
+          image: "public/rma/terreno_alquiler_chorrillos.jpg",
           title: "TERRENO COMERCIAL EN ALQUILER",
           priceSol: "S/. 44,880.00",
           priceUsd: "USD 12,000.00",
@@ -92,7 +92,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-santiago-de-surco-lima-lima-1082047/",
-          image: "src/assets/rma/depa_flat_venta_surco.jpg",
+          image: "public/rma/depa_flat_venta_surco.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 1'365,100.00",
           priceUsd: "USD 365,000.00",
@@ -100,7 +100,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-terreno-en-venta-carabayllo-lima-lima-1080254/",
-          image: "src/assets/rma/terreno_venta_Carabayllo.jpeg",
+          image: "public/rma/terreno_venta_Carabayllo.jpeg",
           title: "TERRENO COMERCIAL EN VENTA",
           priceSol: "S/. 224,400.00",
           priceUsd: "USD 60,000.00",
@@ -108,7 +108,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-terreno-en-alquiler-chorrillos-lima-lima-1080229/",
-          image: "src/assets/rma/terreno_industrial_alquiler_chorrillos.jpg",
+          image: "public/rma/terreno_industrial_alquiler_chorrillos.jpg",
           title: "TERRENO INDUSTRIAL EN ALQUILER",
           priceSol: "S/. 164,560.00",
           priceUsd: "USD 44,000.00",
@@ -116,7 +116,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-santiago-de-surco-lima-lima-1079023/",
-          image: "src/assets/rma/depa_flat_venta_surco2.jpg",
+          image: "public/rma/depa_flat_venta_surco2.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 243,100.00",
           priceUsd: "USD 65,000.00",
@@ -124,7 +124,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-miraflores-lima-lima-1078116/",
-          image: "src/assets/rma/depa_flat_venta_miraflores.jpg",
+          image: "public/rma/depa_flat_venta_miraflores.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 935,000.00",
           priceUsd: "USD 250,000.00",
@@ -132,7 +132,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-lince-lima-lima-1077439/",
-          image: "src/assets/rma/depa_flat_venta_Lince.jpg",
+          image: "public/rma/depa_flat_venta_Lince.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 822,800.00",
           priceUsd: "USD 220,000.00",
@@ -140,7 +140,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-santiago-de-surco-lima-lima-1076943/",
-          image: "src/assets/rma/depa_flat_venta_surco3.jpg",
+          image: "public/rma/depa_flat_venta_surco3.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 411,400.00",
           priceUsd: "USD 110,000.00",
@@ -148,7 +148,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-terreno-en-venta-aguas-verdes-zarumilla-tumbes-1076042/",
-          image: "src/assets/rma/terreno_agricula_venta_tumbes.jpg",
+          image: "public/rma/terreno_agricula_venta_tumbes.jpg",
           title: "TERRENO AGRÍCOLA EN VENTA",
           priceSol: "S/. 6'941,701.00",
           priceUsd: "USD 1'856,070.00",
@@ -156,7 +156,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-casa-en-venta-santiago-de-surco-lima-lima-1075167/",
-          image: "src/assets/rma/casa_urbana_venta_surco.jpg",
+          image: "public/rma/casa_urbana_venta_surco.jpg",
           title: "CASA URBANA EN VENTA",
           priceSol: "S/. 1'492,260.00",
           priceUsd: "USD 399,000.00",
@@ -164,7 +164,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-terreno-en-venta-santa-rosa-lima-lima-1069454/",
-          image: "src/assets/rma/terreno_comercial_venta_santa_rosa.jpg",
+          image: "public/rma/terreno_comercial_venta_santa_rosa.jpg",
           title: "TERRENO COMERCIAL EN VENTA",
           priceSol: "S/. 306,680.00",
           priceUsd: "USD 82,000.00",
@@ -172,7 +172,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-santiago-de-surco-lima-lima-1065659/",
-          image: "src/assets/rma/depa_flat_venta_surco4.jpg",
+          image: "public/rma/depa_flat_venta_surco4.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 1'166,880.00",
           priceUsd: "USD 312,000.00",
@@ -180,7 +180,7 @@ export const people = [
         },
         {
           url: "https://www.remax.pe/web/search/property/propiedad-departamento-en-venta-miraflores-lima-lima-1056803/",
-          image: "src/assets/rma/depa_flat_venta_miraflores_2.jpg",
+          image: "public/rma/depa_flat_venta_miraflores_2.jpg",
           title: "DEPARTAMENTO FLAT EN VENTA",
           priceSol: "S/. 4'114,000.00",
           priceUsd: "USD 1'100,000.00",
